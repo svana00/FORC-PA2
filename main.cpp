f52caa84a4e9dc19f7c229bd9ee9eb613ae84cde
svana00 <54863023+svana00@users.noreply.github.com>
2021-02-09 19:00:14 +0000

    got all the words man

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void pick_word_from_file()
+{
+}
+
+void scramble_word()
+{
+}
+
+void show_scrambled_word()
+{
+}
+
+void guess_word()
+{
+}
 
 int main()
 {
+    // Create array
+    int arrayLength = 20 * 25936;
+    char allWordsArray[arrayLength];
+
+    ifstream fin;
+    fin.open("word_list.fic", ios::binary);
+
+    fin.read(allWordsArray, arrayLength);
+
+    cout << allWordsArray << endl;
+
+    fin.close();
     return 0;
 }
